4b960c1b4d28175b928ddfd49ee7671d490d6bb3
riceczeks <48184368+riceczeks@users.noreply.github.com>
2020-02-07 22:25:28 -0600

    init commit

---
+++ Ch2_exercises.py
@@ -0,0 +1,23 @@
+message = "Hello World!"
+print(message)
+
+mesage = "Hello Python Crash Course World!"
+print(mesage)
+
+string1 = 'I told my friend "Python is my favorite language!"'
+print(string1)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first = "amanda"
+last = "rogers"
+full = f"{first} {last}"
+print(full)
+
+print(f"Hello {full.title()}")
+
+message2 = f"Hello {full.upper()}!"
+print(message2)
