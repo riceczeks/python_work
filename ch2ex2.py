14bfea3fe1362fa9071060a30550de9d2da26e22
riceczeks <48184368+riceczeks@users.noreply.github.com>
2020-02-09 10:36:10 -0600

    completed chapter 2 exercises

---
+++ ch2ex2.py
@@ -0,0 +1,38 @@
+chris = "Hi honey, you're cute!"
+print(chris)
+
+chris2 = "chris rogers"
+print(chris2.upper())
+print(chris2.lower())
+print(chris2.title())
+
+Einstein = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+print(Einstein)
+
+famous_person = Einstein
+print(famous_person)
+
+name = '\t Chris\n Rogers\n'
+print(name)
+name2 = name.rstrip()
+name3 = name.lstrip()
+name4 = name.strip()
+print(name2)
+print(name3)
+print(name4)
+
+age = 14_000_000_0
+print(age)
+
+print(1+7)
+print(10-2)
+print(2*4)
+print(16/2)
+
+num = 15
+
+print(f"My favorite number is {num}")
+
+#This is a code comment and should not print.
+print("Hello")
+
