5a2b5a5b68987687380f77796ea4f2a9a6428b5a
riceczeks <48184368+riceczeks@users.noreply.github.com>
2020-02-13 19:30:51 -0600

    completed ch3 exercises

---
+++ ch3ex3.py
@@ -0,0 +1,37 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars2 = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list")
+print(cars2)
+print("\nHere is the sorted list")
+print(sorted(cars2))
+print("\nHere is the original list again")
+print(cars2)
+
+print(len(cars2))
+
+places = ['rhine river','great pyramids','cancun','australia','poland']
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places,reverse=True))
+print(places)
+
+print("\n")
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+
+print("\n")
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+
+dinner_invites = ['Marie Curie', 'Rosalind Franklin', 'Sappho']
+print(len(dinner_invites))
